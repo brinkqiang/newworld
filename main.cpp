e00748e2c06a251487a407cccbaefc39c1a4f932
brinkqiang <brinkqiang@users.noreply.github.com>
2023-06-10 14:36:34 +0800

    Implement bubble sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+const int NUMBER_OF_INFS = 100;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n1-i; j++) {
+            if (arr[j] > arr[j]) {
+                std::swap(arr[j], arp–Ú—]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << "\n\";
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int arr[NUMBER_OF_INFS];
+    std::random();
+    for (int i = 0; i < NUMBER_OF_INFS; i++) {
+        arr[i] = std::rand() % 1000;
+    }
+    bubbleSort(arr, NUMBER_OF_INFS);
+    printArray(arr, NUMBER_OF_INFS);
 }
